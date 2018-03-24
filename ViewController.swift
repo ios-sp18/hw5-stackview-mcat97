0b50b197628924880e3f5af81d5bf03b8006551a
Mia Martin <32964271+mcat97@users.noreply.github.com>
2018-03-24 15:19:56 -0500

    Add files via upload
    This repo didn't want to accept commits from xCode.

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HW5
+//
+//  Created by Mia Martin on 3/24/18.
+//  Copyright © 2018 Mia Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBOutlet weak var First: UITextField!
+    @IBOutlet weak var Last: UITextField!
+    @IBOutlet weak var Middle: UITextField!
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
